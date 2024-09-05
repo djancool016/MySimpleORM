62def14d11d5d55df5f56581d9cd4353129c2389
djancool016 <dwijulianto16@gmail.com>
2024-09-05 16:25:16 +0700

    skip migration if table already created

--- src/postgres/index.js
+++ src/postgres/index.js
@@ -7,11 +7,21 @@ module.exports = {
         const db = require('./db')
         const {runTruncator, getAllTables} = require('./truncator').init(config)
 
+        const migrations = async (migrations, pool) => {
+            const existingTables = await getAllTables(pool)
+            const migrationsToRun = migrations.filter(migration => !existingTables.includes(migration.tableName))
+            if (migrationsToRun.length > 0) {
+                await runMigrations(migrationsToRun, pool)
+            } else {
+                console.log('All tables already migrated.')
+            }
+        }
+
         return {
             db,
             query,
             getAllTables,
-            runMigrations,
+            runMigrations: migrations,
             runSeed,
             runSeeds,
             runTruncator
