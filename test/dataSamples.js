2fd4f0901d00aeda9aae19e944cd62ba59126a42
djancool016 <dwijulianto16@gmail.com>
2024-09-10 16:20:50 +0700

    test new modelMapper

--- test/dataSamples.js
+++ test/dataSamples.js
@@ -136,7 +136,210 @@ const userModel = {
         }
     ]
 }
+const bookkeepingMigrations = {
+    account: {
+        tableName: "account",
+        timestamp: false,
+        columns: [
+            {
+                columnName: "id",
+                dataType: "INT",
+                nullable: false,
+                autoIncrement: true
+            },
+            {
+                columnName: "description",
+                dataType: "VARCHAR(255)",
+                nullable: false
+            }
+        ]
+    }
+    ,
+    coa: {
+        tableName: "coa",
+        timestamp: false,
+        columns: [
+            {
+                columnName: "id",
+                dataType: "INT",
+                nullable: false,
+                autoIncrement: true
+            },
+            {
+                columnName: "account_id",
+                dataType: "INT",
+                nullable: false,
+                references: {table: 'account', key:'id'}
+            },
+            {
+                columnName: 'code',
+                dataType: 'INT',
+                nullable: false,
+                unique: true
+            },
+            {
+                columnName: "base_value",
+                dataType: "INT",
+                nullable: false
+            },
+            {
+                columnName: "description",
+                dataType: "VARCHAR(255)",
+                nullable: false
+            }
+        ]
+    },
+    register: {
+        tableName: "register",
+        timestamp: false,
+        columns: [
+            {
+                columnName: "id",
+                dataType: "INT",
+                nullable: false,
+                autoIncrement: true
+            },
+            {
+                columnName: "description",
+                dataType: "VARCHAR(255)",
+                nullable: false
+            }
+        ]
+    }
+    ,
+    entry: {
+        tableName: "entry",
+        timestamp: true,
+        columns: [
+            {
+                columnName: "id",
+                dataType: "INT",
+                nullable: false,
+                autoIncrement: true
+            },
+            {
+                columnName: "register_id",
+                dataType: "INT",
+                nullable: false,
+                references: {table:'register', key:'id'}
+            },
+            {
+                columnName: "coa_code",
+                dataType: "INT",
+                nullable: false,
+                references: {table: 'coa', key: 'code'}
+            },
+            {
+                columnName: "dc",
+                dataType: "INT",
+                nullable: false
+            }
+        ]
+    },
+    transaction: {
+        tableName: "transaction",
+        timestamp: true,
+        columns: [
+            {
+                columnName: "id",
+                dataType: "INT",
+                nullable: false,
+                autoIncrement: true
+            },
+            {
+                columnName: "register_id",
+                dataType: "INT",
+                nullable: false,
+                references: {table: 'register', key: 'id'}
+            },
+            {
+                columnName: "amount",
+                dataType: "INT",
+                nullable: false
+            },
+            {
+                columnName: "date",
+                dataType: "DATE",
+                nullable: false
+            },
+            {
+                columnName: "description",
+                dataType: "VARCHAR(255)",
+                nullable: false
+            }
+        ]
+    }
+}
+const transactionModel = {
+    "table": "transaction",
+    "includes": [
+        "id",
+        "register_id",
+        "amount",
+        "date",
+        "description",
+        "created_at",
+        "updated_at"
+    ],
+    "association": [
+        {
+            "table": "register",
+            "references": "register.id",
+            "foreignKey": "transaction.register_id",
+            "includes": [
+                "description"
+            ],
+            "alias": {
+                "description": "register"
+            },
+            "association": []
+        },
+        {
+            "table": "entry",
+            "references": "entry.register_id",
+            "foreignKey": "transaction.register_id",
+            "includes": [
+                "coa_code",
+                "dc",
+            ],
+            "alias": {
+                "coa_code": "coa_code",
+                "dc": "dc"
+            },
+            "association": [
+                {
+                    "table": "coa",
+                    "references": "coa.code",
+                    "foreignKey": "entry.coa_code",
+                    "includes": [
+                        "description"
+                    ],
+                    "alias": {
+                        "description": "coa"
+                    },
+                    "association": [
+                        {
+                            "table": "account",
+                            "references": "account.id",
+                            "foreignKey": "coa.account_id",
+                            "includes": [
+                                "id",
+                                "description"
+                            ],
+                            "alias": {
+                                "id": "account_id",
+                                "description": "account"
+                            },
+                            "association": []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
 
 module.exports = {
-    migrations, seeds, userModel
+    migrations, seeds, userModel, bookkeepingMigrations, transactionModel
 }
