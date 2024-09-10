4bd2cf1760e74b8eca0a1e35ad024ba420c021df
djancool016 <dwijulianto16@gmail.com>
2024-09-10 16:20:20 +0700

    add function to for complex association

--- src/utils/modelMapper.js
+++ src/utils/modelMapper.js
@@ -3,43 +3,99 @@
  * 
  * @param {string} model - The name of the model table to be mapped.
  * @param {object} migrations - An object containing database migration information.
- * @param {object} [includesObj] - An object containing a list of columns to be included in the query for each table.
+ * @param {object} [includeObj] - An object containing a list of columns to be included in the query for each table.
  * 
  * @returns {object} An object containing information about the table, included columns, and associations.
  */
 
-function modelMapper(model = '', migrations, includesObj = {}) {
-    const includes = includesObj[migrations[model].tableName] || migrations[model].columns.map(column => column.columnName)
-    const association = Object.keys(includesObj).length > 0 ? migrations[model].columns
-        .filter(column => column.references)
-        .map(column => {
-            const table = column.references.table
-            const includes = includesObj[table] || []
-            const alias = includes
-                .filter(item => item.includes(':'))
-                .reduce((acc, item) => {
-                    const [key, alias] = item.split(':')
-                    acc[key] = alias
-                    return acc
-                }, {})
-            const association = column.references ? modelMapper(table, migrations, includesObj).association : []
-            return {
-                table,
-                references: `${column.references.table}.${column.references.key}`,
-                foreignKey: `${migrations[model].tableName}.${column.columnName}`,
-                includes: includes.map(item => item.split(':')[0]),
-                alias,
-                association
+function modelMapper(model = '', migrations, includeObj = {}) {
+
+    const includes = includeObj[migrations[model].tableName] || migrations[model].columns.map(column => column.columnName)
+
+    const associationBuilder = (tableName) => {
+        return migrations[tableName].columns
+            .filter(column => column.references)
+            .map(column => {
+                const table = column.references.table
+                const includes = includeObj[table] || []
+                const alias = includes
+                    .filter(item => item.includes(':'))
+                    .reduce((acc, item) => {
+                        const [key, alias] = item.split(':')
+                        acc[key] = alias
+                        return acc
+                    }, {})
+                const association = column.references ? associationBuilder(table) : []
+                return {
+                    table,
+                    references: `${column.references.table}.${column.references.key}`,
+                    foreignKey: `${migrations[tableName].tableName}.${column.columnName}`,
+                    includes: includes.map(item => item.split(':')[0]),
+                    alias,
+                    association
+                }
+            })
+    }
+    const includesReferences = () => {
+        const ref = (tableName) => migrations[tableName].columns.filter(column => column.references).map(column => column.references)
+        
+        return Object.keys(includeObj).flatMap(tableName => {
+            if (tableName != model && containsSubset(ref(tableName), ref(model))) {
+                return migrations[model].columns
+                    .filter(column => column.references)
+                    .map(column => {
+                        const table = tableName
+                        const includes = includeObj[table] || []
+                        const alias = includes
+                            .filter(item => item.includes(':'))
+                            .reduce((acc, item) => {
+                                const [key, alias] = item.split(':')
+                                acc[key] = alias
+                                return acc
+                            }, {})
+                        const association = column.references ? associationBuilder(table) : []
+                        association.forEach((assoc, index) => {
+                            if(ref(model).some(({table, key}) => assoc.table === table )){
+                                association.splice(index, 1)
+                            }
+                        })
+                        return {
+                            table,
+                            references: `${tableName}.${column.columnName}`,
+                            foreignKey: `${model}.${column.columnName}`,
+                            includes: includes.map(item => item.split(':')[0]),
+                            alias,
+                            association
+                        }
+                    })
             }
-        }) : []
-        if(!includesObj[migrations[model].tableName] && migrations[model].timestamp){
-            includes.push('created_at', 'updated_at')
-        }
+            return null  // To avoid returning undefined if the condition is not met
+        }).filter(Boolean)  // Removes any null values
+    }
+
+    const association = Object.keys(includeObj).length > 0 ? associationBuilder(model).concat(...includesReferences()) : []
+
+    // push includeReferences to association
+
+    if(!includeObj[migrations[model].tableName] && migrations[model].timestamp){
+        includes.push('created_at', 'updated_at')
+    }
     return {
         table: migrations[model].tableName,
         includes,
         association
     }
+}
+
+function isEqual(obj1, obj2) {
+    return Object.keys(obj1).length === Object.keys(obj2).length &&
+           Object.keys(obj1).every(key => obj1[key] === obj2[key])
+}
+
+function containsSubset(mainArray, subsetArray) {
+    return subsetArray.every(subsetItem =>
+        mainArray.some(mainItem => isEqual(mainItem, subsetItem))
+    )
 }
 
 module.exports = modelMapper
--- test/modelMapper.test.js
+++ test/modelMapper.test.js
@@ -1,116 +1,16 @@
 const config = require('../config')
 const {builder: {queryBuilder}, modelMapper} = require('../src').init(config)
+const {bookkeepingMigrations: migrations} = require('./dataSamples')
 
-const migrations = {
-    account: {
-        tableName: "account",
-        timestamp: false,
-        columns: [
-            {
-                columnName: "id",
-                dataType: "INT",
-                nullable: false,
-                autoIncrement: true
-            },
-            {
-                columnName: "description",
-                dataType: "VARCHAR(255)",
-                nullable: false
-            }
-        ]
-    }
-    ,
-    coa: {
-        tableName: "coa",
-        timestamp: false,
-        columns: [
-            {
-                columnName: "id",
-                dataType: "INT",
-                nullable: false,
-                autoIncrement: true
-            },
-            {
-                columnName: "account_id",
-                dataType: "INT",
-                nullable: false,
-                references: {table: 'account', key:'id'}
-            },
-            {
-                columnName: 'code',
-                dataType: 'INT',
-                nullable: false,
-                unique: true
-            },
-            {
-                columnName: "base_value",
-                dataType: "INT",
-                nullable: false
-            },
-            {
-                columnName: "description",
-                dataType: "VARCHAR(255)",
-                nullable: false
-            }
-        ]
-    },
-    register: {
-        tableName: "register",
-        timestamp: false,
-        columns: [
-            {
-                columnName: "id",
-                dataType: "INT",
-                nullable: false,
-                autoIncrement: true
-            },
-            {
-                columnName: "description",
-                dataType: "VARCHAR(255)",
-                nullable: false
-            }
-        ]
-    }
-    ,
-    entry: {
-        tableName: "entry",
-        timestamp: false,
-        columns: [
-            {
-                columnName: "id",
-                dataType: "INT",
-                nullable: false,
-                autoIncrement: true
-            },
-            {
-                columnName: "register_id",
-                dataType: "INT",
-                nullable: false,
-                references: {table:'register', key:'id'}
-            },
-            {
-                columnName: "coa_code",
-                dataType: "INT",
-                nullable: false,
-                references: {table: 'coa', key: 'code'}
-            },
-            {
-                columnName: "dc",
-                dataType: "INT",
-                nullable: false
-            }
-        ]
-    }
-}
-
-const includesObj = {
-    register: ['description:register'],
-    coa: ['description:coa'],
-    account: ['description:account']
+const includeColumns = {
+    register: ['id:register_id'],
+    entry: ['id:entry_id'],
+    coa: ['code: coa_code'],
+    account: ['id:account_id']
 }
 
 test('test modelMapper', () => {
-    const result = modelMapper('entry', migrations, includesObj, false)
+    const result = modelMapper('transaction', migrations, includeColumns)
     const query = queryBuilder(result)
     console.log(JSON.stringify(result, null, 4))
     console.log(query.read({id: 1}))
